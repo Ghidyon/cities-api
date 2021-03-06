5865fe3bad7dea0fca0d91b2bbc0161a3459ea10
Ghidyon <gideonakunana@gmail.com>
2021-03-06 14:45:01 +0100

    fetch data

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const cities = [];
+
+// fetch() returns a promise
+fetch(endpoint)
+    .then(response => response.json())
+    .then(data => cities.push(...data));
+
+console.log(cities);
